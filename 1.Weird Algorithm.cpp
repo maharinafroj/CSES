f5d94e0df1ba8c449ed7bfb2fb1bd54f8a51f2a4
maharin_afroj <74603459+maharinafroj@users.noreply.github.com>
2022-06-19 01:27:09 +0600

    Create 1.Weird Algorithm.cpp

---
+++ 1.Weird Algorithm.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+int main()
+{
+    ll n;
+    cin>>n;
+    cout<<n<<" ";
+    while(n>1)
+
+   {
+       if(n%2==0)
+    {
+        n/=2;
+    }
+    else
+    {
+        n=n*3+1;
+    }
+    cout<<n<<" ";
+   }
+   cout<<endl;
+   return 0;
+}
